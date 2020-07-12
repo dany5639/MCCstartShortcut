4cfe7658f55a1f6389bc0767e44e0ca9ff536cef
dany5639 <dany_woozie@hotmail.com>
2020-07-12 12:51:15 +0200

    Simplified and hardcoded appID for a VirusTotal test.

--- MCC/Program.cs
+++ MCC/Program.cs
@@ -1,2 +1,16 @@
 ﻿using System.Diagnostics;
-namespace MCC{class Program{static void Main(string[] args){ if (args.Length != 2) return; Process.Start($"steam://launch/{args[0]}/option{args[1]}");}}}
+namespace MCC
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            // if (args.Length != 2)
+            //     return;
+            Process.Start($"steam://launch/976730/option0");
+            // 976730 0
+            // Process.Start($"steam://launch/{args[0]}/option{args[1]}");
+            // arg0 is the game steam appID; arg1 is the launch option, most games don't use it. 
+            // for MCC, option0 is retail, option1 is EAC disabled
+        }
+    }
+}
